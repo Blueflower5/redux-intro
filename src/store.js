2a030dee32508864cfef6b97b35d3b815d7d480e
Blueflower5 <mohammad9776@gmail.com>
2025-04-06 01:32:27 +0430

    c20-7

--- src/store.js
+++ src/store.js
@@ -1,10 +1,15 @@
-import { createStore } from "redux";
-const initalState = {
+import { combineReducers, createStore } from "redux";
+const initalStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-function reducer(state = initalState, action) {
+const initialStateCustomer = {
+  name: "",
+  id: "",
+  time: "",
+};
+function reducerAccount(state = initalStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -30,7 +35,27 @@ function reducer(state = initalState, action) {
   }
 }
 
-const store = createStore(reducer);
+function reducerCustomer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        name: action.payload.name,
+        id: action.payload.id,
+        time: action.payload.time,
+      };
+    case "customer/updateName":
+      return { ...state, name: action.payload };
+    default:
+      return state;
+  }
+}
+const rootReducer = combineReducers({
+  account: reducerAccount,
+  customer: reducerCustomer,
+});
+
+const store = createStore(rootReducer);
 // store.dispatch({ type: "account/deposit", payload: 1000 });
 // store.dispatch({ type: "account/withdraw", payload: 800 });
 // store.dispatch({
@@ -53,4 +78,17 @@ function payLoan() {
 
 store.dispatch(payLoan());
 
+console.log(store.getState());
+
+function createCustomer(name, id) {
+  return {
+    type: "customer/createCustomer",
+    payload: { name, id, time: new Date().toISOString() },
+  };
+}
+function updateName(name) {
+  return { type: "customer/updateName", payload: name };
+}
+
+store.dispatch(createCustomer("John Doe", "12345"));
 console.log(store.getState());

7eddcdcbe3cfab1cb0e028d32b830c37d5e77f3c
Blueflower5 <mohammad9776@gmail.com>
2025-04-05 23:16:19 +0430

    c20-4

---
+++ src/store.js
@@ -0,0 +1,25 @@
+const initalState = {
+  balance: 0,
+  loan: 0,
+  loanPurspose: "",
+};
+function reducer(state = initalState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.load > 0) return state;
+      return { ...state };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
